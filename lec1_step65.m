9b75963a7adacfa7b087a262aaad960ac30d336d
Hiro <hiro@HiroWaga-Retina-MacBook-Pro-2.local>
2020-04-10 15:10:51 +0900

    Update lec1_step65.m

--- lec1_step65.m
+++ lec1_step65.m
@@ -1,6 +1,8 @@
-%% /* The MIT License (MIT)
-% 
-% Copyright (c) 2020 Hiroaki Wagatsuma and Wagatsuma Lab@Kyutech.
+%% MATLAB programming course for beginners, supported by Wagatsuma Lab@Kyutech 
+%
+% /* 
+% The MIT License (MIT): 
+% Copyright (c) 2020 Hiroaki Wagatsuma and Wagatsuma Lab@Kyutech
 % 
 % Permission is hereby granted, free of charge, to any person obtaining a
 % copy of this software and associated documentation files (the
@@ -20,13 +22,15 @@
 % DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 % OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 % USE OR OTHER DEALINGS IN THE SOFTWARE. */
-%%
+%% Specifications and requirements
 % # @Time    : 2020-4-20 
 % # @Author  : Hiroaki Wagatsuma
 % # @Site    : https://github.com/hirowgit/1_matlab_basic_course
-% # @File    : lec1_step65.m
 % # @IDE     : MATLAB R2018a
-%%
+% # @File    : lec1_step65.m
+
+%%  Main program
+
 dt=0.01; noise_level=0.1;
 t=0:dt:2*pi;
 dataUpper=noise_level.*rand(1,length(t));
@@ -39,6 +43,7 @@ plot(t,sin(t),'b.-'), hold on;
 plot(t,dataUpper+sin(t),'r.');
 grid on;
 
+
 subplot(3,1,2);
 % figure(3); clf;
 plot(t,dataUpper,'.','MarkerSize',15);
@@ -49,3 +54,15 @@ set(gca,'ylim',[0 noise_level],'FontSize',14,'Color',[1 1 0],'FontName','Helveti
 subplot(3,1,3);
 % figure(3); clf;
 plot(t,dataUpDown,'.','MarkerSize',15);
+
+
+%% Supplementary information to publish
+%
+% If you want to make a pdf or html file on the code, 
+% you can use the code "x_publish_each_codes.m" in the same folder. 
+% Please change the file name as " this_file_tag='lec*_step*' "  
+% (* will be replaced to the number of the target file). 
+%
+% The code "x_publish_all_codes.m" works for such a publication applying to all
+% codes in the same folder (Note: "x_publish_all_codes_sub.m" should be located in the same folder).
+%%
