8e7d704df23aeb2a3372e8ba0c49aafcb280ca34
Hiro <hiro@HiroWaga-Retina-MacBook-Pro-2.local>
2020-04-10 15:10:53 +0900

    Update lec1_step62.m

--- lec1_step62.m
+++ lec1_step62.m
@@ -1,6 +1,8 @@
-%% /* The MIT License (MIT)
-% 
-% Copyright (c) 2020 Hiroaki Wagatsuma and Wagatsuma Lab@Kyutech.
+%% MATLAB programming course for beginners, supported by Wagatsuma Lab@Kyutech 
+%
+% /* 
+% The MIT License (MIT): 
+% Copyright (c) 2020 Hiroaki Wagatsuma and Wagatsuma Lab@Kyutech
 % 
 % Permission is hereby granted, free of charge, to any person obtaining a
 % copy of this software and associated documentation files (the
@@ -20,13 +22,16 @@
 % DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 % OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 % USE OR OTHER DEALINGS IN THE SOFTWARE. */
-%%
+%% Specifications and requirements
 % # @Time    : 2020-4-20 
 % # @Author  : Hiroaki Wagatsuma
 % # @Site    : https://github.com/hirowgit/1_matlab_basic_course
-% # @File    : lec1_step62.m
 % # @IDE     : MATLAB R2018a
-%%
+% # @File    : lec1_step62.m
+
+%%  Main program
+
+
 dt=0.01; noise_level=0.1;
 t=0:dt:2*pi;
 data=noise_level.*rand(1,length(t));
@@ -37,3 +42,15 @@ plot(t,sin(t),'b.-'), hold on;
 % figure(2); clf;
 plot(t,data+sin(t),'r.');
 grid on;
+
+
+%% Supplementary information to publish
+%
+% If you want to make a pdf or html file on the code, 
+% you can use the code "x_publish_each_codes.m" in the same folder. 
+% Please change the file name as " this_file_tag='lec*_step*' "  
+% (* will be replaced to the number of the target file). 
+%
+% The code "x_publish_all_codes.m" works for such a publication applying to all
+% codes in the same folder (Note: "x_publish_all_codes_sub.m" should be located in the same folder).
+%%
